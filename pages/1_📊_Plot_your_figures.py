8f52ecaf7c5bbe082cf6eac238a84633bbbf4199
Rosetung <110995636+Rosetung@users.noreply.github.com>
2023-10-19 19:58:08 +0800

    Update 1_📊_Plot_your_figures.py

--- pages/1_📊_Plot_your_figures.py
+++ pages/1_📊_Plot_your_figures.py
@@ -320,8 +320,8 @@ with tab3:
             
         with c3:
             st.markdown("Normality test")
-            figp = pg.qqplot(fsdf[quant], dist='norm')
-            st.pyplot(ggplot.draw(figp))
+            p = pg.qqplot(fsdf[quant], dist='norm')
+            st.pyplot(p)
             ddd = shapiro(fsdf[quant])[1]
             st.write("Shapiro p-Value: " + str(ddd))
             if ddd<0.05:
